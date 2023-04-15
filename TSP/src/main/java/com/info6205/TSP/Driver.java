506c8a27b3399e48ebb0a751445ae54fe3f01107
Vipul Rajderkar <vipulrajderkar@Vipuls-MacBook-Pro.local>
2023-04-15 19:41:20 -0400

    Updated Eulerian implentation

--- TSP/src/main/java/com/info6205/TSP/Driver.java
+++ TSP/src/main/java/com/info6205/TSP/Driver.java
@@ -2,6 +2,7 @@ package com.info6205.TSP;
 
 import com.info6205.TSP.algorithms.Christofides;
 import com.info6205.TSP.graph.City;
+import com.info6205.TSP.graph.Graph;
 import com.info6205.TSP.ui.ViewTSPAlgo;
 
 import java.io.File;
@@ -11,6 +12,24 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Driver {
+
+
+    public static double calculateTourDistance(List<City> tour) {
+        double totalDistance = 0.0;
+        Graph graph = new Graph(tour);
+        for (int i = 0; i < tour.size() - 1; i++) {
+            City cityA = tour.get(i);
+            City cityB = tour.get(i + 1);
+            double distance = graph.distance(cityA, cityB);
+            totalDistance += distance;
+        }
+        // Add distance from last city to first city to complete the tour
+        City firstCity = tour.get(0);
+        City lastCity = tour.get(tour.size() - 1);
+        double distance = graph.distance(lastCity, firstCity);
+        totalDistance += distance;
+        return totalDistance;
+    }
 
 
     public static List<City> readCitiesFromFile(String filename) throws FileNotFoundException {
@@ -45,30 +64,17 @@ public class Driver {
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-        /*
-        cities.add(new City("New York", 40.712776, -74.005974));
-        cities.add(new City("Chicago", 41.878114, -87.629798));
-        cities.add(new City("Houston", 29.760427, -95.369803));
-        cities.add(new City("Phoenix", 33.448376, -112.074036));
-        cities.add(new City("Philadelphia", 39.952584, -75.165222));
-        cities.add(new City("San Antonio", 29.424122, -98.493628));
-        cities.add(new City("Los Angeles", 34.052235, -118.243683));
-        cities.add(new City("San Diego", 32.715736, -117.161087));
-        cities.add(new City("Dallas", 32.776665, -96.796989));
-        cities.add(new City("San Jose", 37.338207, -121.886330));
-        cities.add(new City("Austin", 30.267153, -97.743057));
-        cities.add(new City("Jacksonville", 30.332184, -81.655647));
-        cities.add(new City("Fort Worth", 32.755489, -97.330765));
-        cities.add(new City("Columbus", 39.961178, -82.998795));
-        cities.add(new City("San Francisco", 37.774929, -122.419418));
-        */
-
+        //480
         Christofides christofides = new Christofides(cities);
         christofides.findTour();
         List<City> tour = christofides.getTour();
-        for (City city : tour) {
-            System.out.println(city.id + " " + city.longitude + " " + city.longitude);
-        }
+
+        // Calculate the total tour distance in meters
+        double totalDistance = calculateTourDistance(tour);
+
+        // Print the tour and total distance
+//        System.out.println("Tour: " + tour);
+        System.out.println("Total Distance: " + totalDistance + " meters");
 
         ViewTSPAlgo view =  new ViewTSPAlgo(tour);
         view.setVisible(true);
