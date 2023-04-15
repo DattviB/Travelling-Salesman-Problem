467f7909a6ee0d15a149006e454ab74e6e2ade04
DattviB <bhatt.da@northeastern.edu>
2023-04-14 23:32:03 -0400

    Added city class

---
+++ TSP/src/main/java/com/info6205/TSP/graph/City.java
@@ -0,0 +1,13 @@
+package com.info6205.TSP.graph;
+
+public class City {
+    public String id;
+    public double lat;
+    public double lon;
+
+    public City(String id, double lat, double lon) {
+        this.id = id;
+        this.lat = lat;
+        this.lon = lon;
+    }
+}
